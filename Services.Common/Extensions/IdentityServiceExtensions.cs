70afb3d016d3dd2f0eff64eb059a54a7b0aeeb07
Ivan Fernando Paz <ifer343@gmail.com>
2024-07-11 23:07:59 -0500

    adding strict identity jwt settings

--- Services.Common/Extensions/IdentityServiceExtensions.cs
+++ Services.Common/Extensions/IdentityServiceExtensions.cs
@@ -25,12 +25,44 @@ public static class IdentityServiceExtensions
                     ValidateLifetime = true,
                     ValidateIssuerSigningKey = true,
                     ClockSkew = TimeSpan.FromSeconds(0),
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Token:Key"] ?? throw new InvalidOperationException()))
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:SecurityKey"] ?? throw new InvalidOperationException()))
                 };
             });
 
         services.AddAuthorization();
         services.AddAuthorizationBuilder();
+        return services;
+    }
+    public static IServiceCollection AddIdentityServiceStrict(this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    RequireExpirationTime = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = config["JwtSettings:Issuer"],
+                    ValidAudience = config["JwtSettings:Audience"],
+                    ClockSkew = TimeSpan.FromSeconds(0),
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:SecurityKey"] ?? throw new InvalidOperationException()))
+                };
+            });
+
+
+        services.AddAuthorization();
+
+        services.AddAuthorizationBuilder();
+
+
         return services;
     }
 }

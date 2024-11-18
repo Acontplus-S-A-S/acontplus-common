8bbb2505b18b76a482bc55eaf8d58f3b3e139bd8
Ivan Fernando Paz <ifer777@outlook.com>
2024-11-18 00:07:54 -0500

    update barcode generation utils

--- Common.Core/Utils/BarcodeGen.cs
+++ Common.Core/Utils/BarcodeGen.cs
@@ -1,43 +1,55 @@
-﻿using BarcodeStandard;
-using Type = BarcodeStandard.Type;
+﻿using SkiaSharp;
+using ZXing;
+using ZXing.Common;
+using ZXing.SkiaSharp.Rendering;
 
-namespace Common.Core.Utils;
-
-public static class BarcodeGen
+namespace Common.Core.Utils
 {
-    public static byte[] Create(string text, bool label = false)
+    public class BarcodeConfig
     {
-        if (string.IsNullOrEmpty(text))
+        public string Text { get; set; } = string.Empty; // The text to encode
+        public BarcodeFormat Format { get; set; } = BarcodeFormat.CODE_128; // Barcode format
+        public int Width { get; set; } = 300; // Barcode width
+        public int Height { get; set; } = 100; // Barcode height
+        public bool IncludeLabel { get; set; } = false; // Include label below the barcode
+        public int Margin { get; set; } = 10; // Margin around the barcode
+        public SKEncodedImageFormat OutputFormat { get; set; } = SKEncodedImageFormat.Png; // Output image format
+        public int Quality { get; set; } = 100; // Image quality (for formats like JPEG)
+        public EncodingOptions AdditionalOptions { get; set; } // Custom encoding options
+    }
+
+    public static class BarcodeGen
+    {
+        public static byte[] Create(BarcodeConfig config)
         {
-            return null;
+            if (string.IsNullOrEmpty(config.Text))
+                throw new ArgumentException("Text cannot be null or empty", nameof(config.Text));
+
+            // Merge default options with additional options
+            var options = config.AdditionalOptions ?? new EncodingOptions();
+            options.Width = config.Width;
+            options.Height = config.Height;
+            options.Margin = config.Margin;
+            options.PureBarcode = !config.IncludeLabel;
+
+            // Create a barcode writer
+            var writer = new BarcodeWriter<SKBitmap>
+            {
+                Format = config.Format,
+                Options = options,
+                Renderer = new SKBitmapRenderer()
+            };
+
+            // Generate the barcode
+            var barcodeBitmap = writer.Write(config.Text);
+
+            // Encode the barcode image
+            using var image = SKImage.FromBitmap(barcodeBitmap);
+            using var data = image.Encode(config.OutputFormat, config.Quality);
+            using var memoryStream = new MemoryStream();
+            data.SaveTo(memoryStream);
+
+            return memoryStream.ToArray();
         }
-
-        using var memoryStream = new MemoryStream();
-        var b = new Barcode();
-        b.IncludeLabel = label;
-        b.Encode(Type.Code128, text, 1000, 100);
-        b.SaveImage(memoryStream, SaveTypes.Png);
-        return memoryStream.ToArray();
-
-
-        //var writer = new ZXing.Windows.Compatibility.BarcodeWriter
-        //{
-        //    Format = BarcodeFormat.CODE_128,
-        //    Options = new EncodingOptions
-        //    {
-        //        Width = 300,
-        //        Height = 100,
-        //        PureBarcode = label,
-        //    },
-        //};
-
-        //var bitmap = writer.Write(text);
-
-        //using (var memoryStream = new MemoryStream())
-        //{
-        //    bitmap.Save(memoryStream, ImageFormat.Png);
-
-        //    return memoryStream.ToArray();
-        //}
     }
 }

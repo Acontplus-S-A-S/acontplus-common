7d894396199066bff5c84df2b114dec807be1405
Ivan Fernando Paz <ifer777@outlook.com>
2024-11-18 00:08:25 -0500

    adding common test api

---
+++ Common.TestApi/Controllers/BarcodeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using Common.Core.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Common.TestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BarcodeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var barcodeConfig = new BarcodeConfig
+            {
+                Text = "0605202201030150819800120010030000012904948150712",
+                Format = ZXing.BarcodeFormat.QR_CODE
+            };
+            var barcode = BarcodeGen.Create(barcodeConfig);
+            return File(barcode, "image/png", "ci.png");
+        }
+    }
+}

28aa6f22dc668a59115735c531285b0a944bd6f9
Ivan Fernando Paz <ifer777@outlook.com>
2024-11-27 16:18:50 -0500

    adding acontplus common api documentation nuget

---
+++ Common.ApiDocumentation/ConfigureSwaggerOptions.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Common.ApiDocumentation;
+public class ConfigureSwaggerOptions : IConfigureNamedOptions<SwaggerGenOptions>
+{
+    private readonly IApiVersionDescriptionProvider _provider;
+
+    public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider)
+    {
+        _provider = provider;
+    }
+
+    public void Configure(SwaggerGenOptions options)
+    {
+        foreach (var description in _provider.ApiVersionDescriptions)
+        {
+            options.SwaggerDoc(description.GroupName, CreateVersionInfo(description));
+        }
+    }
+
+    public void Configure(string name, SwaggerGenOptions options)
+    {
+        Configure(options);
+    }
+
+    private OpenApiInfo CreateVersionInfo(ApiVersionDescription description)
+    {
+        var assembly = Assembly.GetEntryAssembly();
+        var assemblyName = assembly?.GetName().Name;
+        var info = new OpenApiInfo { Title = $"{assemblyName}", Version = description.ApiVersion.ToString() };
+        if (description.IsDeprecated)
+        {
+            info.Description += " This API version has been deprecated. Please use one of the new APIs available from the explorer.";
+        }
+        return info;
+    }
+}
---
+++ Common.ApiDocumentation/SwaggerAndVersioningExtensions.cs
@@ -0,0 +1,73 @@
+﻿using Asp.Versioning;
+using Asp.Versioning.ApiExplorer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Common.ApiDocumentation;
+
+public static class SwaggerAndVersioningExtensions
+{
+    public static IServiceCollection AddVersioningAndSwagger(this IServiceCollection services)
+    {
+        services.AddApiVersioning(opt =>
+        {
+            opt.DefaultApiVersion = new ApiVersion(1, 0);
+            opt.AssumeDefaultVersionWhenUnspecified = true;
+            opt.ReportApiVersions = true;
+            opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                new QueryStringApiVersionReader("api-version"),
+                new HeaderApiVersionReader("x-api-version"),
+                new MediaTypeApiVersionReader("x-api-version"));
+        }).AddApiExplorer(setup =>
+        {
+            setup.GroupNameFormat = "'v'VVV";
+            setup.SubstituteApiVersionInUrl = true;
+        });
+
+        services.ConfigureOptions<ConfigureSwaggerOptions>();
+
+        return services;
+    }
+    public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+    {
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            var securitySchema = new OpenApiSecurityScheme
+            {
+                Description = "JWT Auth Bearer Scheme",
+                Name = "Authorisation",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer",
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+            };
+
+            c.AddSecurityDefinition("Bearer", securitySchema);
+
+            var securityRequirement = new OpenApiSecurityRequirement { { securitySchema, new[] { "Bearer" } } };
+
+            c.AddSecurityRequirement(securityRequirement);
+        });
+        return services;
+    }
+    public static IApplicationBuilder UseSwaggerAndVersioning(this IApplicationBuilder app)
+    {
+        var apiVersionDescriptionProvider =
+            app.ApplicationServices.GetRequiredService<IApiVersionDescriptionProvider>();
+
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions)
+            {
+                options.SwaggerEndpoint($"/openapi/{description.GroupName}.json",
+                    description.GroupName.ToUpperInvariant());
+            }
+        });
+
+        return app;
+    }
+}

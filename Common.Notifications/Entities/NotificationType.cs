b098c48154ea0fcf9fefd0e52f57de35e5b199e7
Ivan Fernando Paz <ifer777@outlook.com>
2024-12-16 17:15:49 -0500

    renaming table name

--- Common.Notifications/Entities/NotificationType.cs
+++ Common.Notifications/Entities/NotificationType.cs
@@ -1,5 +1,5 @@
 ﻿namespace Common.Notifications.Entities;
-[Table("Notification", Schema = "Common")]
+[Table("NotificationType", Schema = "Common")]
 public class NotificationType : BaseEntity
 {
     [Required, MaxLength(5)] public string Code { get; set; }

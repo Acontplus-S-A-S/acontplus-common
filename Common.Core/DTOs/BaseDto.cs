7d07ba12dea8e587091a9949a81db6f11ee066e1
Ivan Fernando Paz <ifer777@outlook.com>
2025-04-20 10:08:24 -0500

    Update package versions and add BaseDto class
    - Updated `Acontplus.Common.Core` to version `1.3.8`.
    - Added new `BaseDto` class with nullable properties: `UserId`, `UserRoleId`, `Enabled`, and `FromMobile`.
    - Updated `Acontplus.Common.Notifications` to version `0.1.15`.
    - Updated `Acontplus.Common.S3Application` to version `1.1.16`.
    - Updated `AWSSDK.Core` to version `3.7.402.43` and `AWSSDK.S3` to version `3.7.416.13`.
    - Updated `Scriban` to version `6.2.1`.

---
+++ Common.Core/DTOs/BaseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.Core.DTOs;
+
+public class BaseDto
+{
+    public int? UserId { get; set; }
+    public int? UserRoleId { get; set; }
+    public bool? Enabled { get; set; }
+    public bool? FromMobile { get; set; }
+}
+

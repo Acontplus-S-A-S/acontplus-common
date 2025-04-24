4b3b695d40ed1c1e683f4106ee155f00bf528bf9
Ivan Fernando Paz <ifer777@outlook.com>
2025-04-23 22:24:35 -0500

    Update version and add pagination support
    - Bump version from 1.3.9 to 1.3.10 in Common.Core.csproj.
    - Introduce PaginationDto class for pagination properties.
    - Enhance ApiResponse class with constructors and static methods for success and error responses, including a generic version.
    - Add PagedResult<T> class for handling paginated results.

---
+++ Common.Core/DTOs/PaginationDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Common.Core.DTOs;
+
+public class PaginationDto
+{
+    // Basic pagination
+    public int PageIndex { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+
+    // User context
+    public int? UserId { get; set; }
+
+    // Search and filtering
+    public string TextSearch { get; set; }
+    public bool? IsEnabled { get; set; }
+
+    // Sorting
+    public string SortBy { get; set; }
+    public bool SortDescending { get; set; } = false;
+}
---
+++ Common.Core/Models/PagedResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.Core.Models;
+
+public class PagedResult<T>
+{
+    public IEnumerable<T> Items { get; set; }
+    public int PageIndex { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+}

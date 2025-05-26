833560d294e1d3e670b042ef15ef2feb268f420d
Ivan Fernando Paz <ifer777@outlook.com>
2025-05-25 22:51:01 -0500

    adding properties

--- Common.Core/DTOs/BaseDto.cs
+++ Common.Core/DTOs/BaseDto.cs
@@ -4,7 +4,9 @@ public class BaseDto
 {
     public int? UserId { get; set; }
     public int? UserRoleId { get; set; }
-    public bool? Enabled { get; set; }
+    public bool? Enabled { get; set; } = true; // Deprecated, use IsActive instead
+    public bool? IsActive { get; set; } = true;
+    public bool? IsDeleted { get; set; }
     public bool? FromMobile { get; set; }
 }
 

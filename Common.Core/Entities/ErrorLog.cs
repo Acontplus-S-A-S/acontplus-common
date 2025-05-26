2216a65567420812fc597cc07d779717129502e0
Ivan Fernando Paz <ifer777@outlook.com>
2025-05-26 17:15:28 -0500

    Update package versions and modify ErrorLog class
    - Bump `Acontplus.Common.Core` version to `1.4.4`.
    - Remove max length attributes from properties in `ErrorLog`.
    - Add `AdditionalInfo` property to `ErrorLog` class.
    - Update `Acontplus.Common.Infrastructure` version to `1.2.18`.

--- Common.Core/Entities/ErrorLog.cs
+++ Common.Core/Entities/ErrorLog.cs
@@ -5,16 +5,12 @@ namespace Common.Core.Entities;
 public class ErrorLog : BaseEntity
 {
     public Guid Token { get; set; }
-
     [MaxLength(100)] public string ErrorMethod { get; set; }
-
     public int ErrorNumber { get; set; }
     public int ErrorSeverity { get; set; }
     public int ErrorState { get; set; }
-
     [MaxLength(150)] public string ErrorProcedure { get; set; }
-
     public int ErrorLine { get; set; }
-
     [MaxLength(300)] public string ErrorMessage { get; set; }
+    public string AdditionalInfo { get; set; }
 }

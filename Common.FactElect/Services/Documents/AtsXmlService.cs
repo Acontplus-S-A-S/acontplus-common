ca47882898a1b9d76ad6862fab818cb4e70f8245
Ivan Fernando Paz <ifer777@outlook.com>
2025-07-31 00:38:52 -0500

    Update version to 0.4.6 and enhance payment method handling in AtsXmlService
    - Bump version in `Common.FactElect.csproj` from 0.4.5 to 0.4.6.
    - Modify `AtsXmlService` to conditionally write payment method elements only if `FormaPago` is not null or empty, improving XML output integrity.

--- Common.FactElect/Services/Documents/AtsXmlService.cs
+++ Common.FactElect/Services/Documents/AtsXmlService.cs
@@ -219,9 +219,12 @@ public class AtsXmlService : IAtsXmlService
             xtr.WriteElement("valorRetIva", sale.ValorRetIva);
             xtr.WriteElement("valorRetRenta", sale.ValorRetRenta);
 
-            xtr.WriteStartElement("formasDePago");
-            xtr.WriteElement("formaPago", sale.FormaPago);
-            xtr.WriteEndElement(); // end formasDePago
+            if (!string.IsNullOrEmpty(sale.FormaPago))
+            {
+                xtr.WriteStartElement("formasDePago");
+                xtr.WriteElement("formaPago", sale.FormaPago);
+                xtr.WriteEndElement(); // end formasDePago
+            }
 
             xtr.WriteEndElement(); // end detalleVentas
         }
